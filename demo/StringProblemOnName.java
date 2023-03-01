4de949c9b5673a8a22d6dfebdc9afdbdd47fea7c
Somashekhar <somashekharm4159@gmail.com>
2023-03-01 18:29:14 +0530

    Started with name Sloving Problem

---
+++ demo/StringProblemOnName.java
@@ -0,0 +1,14 @@
+package com.uis.demo;
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+public class StringProblemOnName {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter name : ");
+		String name = kb.nextLine();
+		
+	}
+
+}
